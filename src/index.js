62879edb37e02962fe66cb6828c1f206b58f1c11
hosea <starit@msn.cn>
2019-03-14 11:03:10 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import _ from 'lodash';
 import './assets/styles/hello.css';
 import icon from './assets/images/icon.png';
 import xml from './assets/datas/xml/hi.xml';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
-  
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
     element.classList.add('hello');
 
     var myIcon = new Image();
@@ -16,6 +17,11 @@ function component() {
     element.appendChild(myIcon);
 
     console.log(xml);
+    
+    btn.innerHTML = '点击这里，然后查看 console！';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
   }
